2c5bac1b475d760360991edce084dbb5f8127886
4daymond <135753656+4daymond@users.noreply.github.com>
2023-10-12 12:21:53 -0700

    the switch

--- App.js
+++ App.js
@@ -1,35 +1,27 @@
 import React from "react"
-import Contact from "./Contact"
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+import Card from "./components/Card"
+import data from "./data"
 
-function App() {
+export default function App() {
+    const cards = data.map(item => {
+        return (
+            <Card
+                key={item.id}
+                {...item}
+                
+            />
+        )
+    })        
+    
     return (
-        <div className="contacts">
-            <Contact 
-                img="./images/junglefeets.jpg" 
-                name="Mr. Feetsiez"
-                phone="(212) 555-1234"
-                email="mr.whiskaz@catnap.meow"
-            />
-            <Contact 
-                img="./images/fluffykins.png"
-                name="Fluffykins"
-                phone="(212) 555-2345"
-                email="fluff@me.com"
-            />
-            <Contact 
-                img="./images/felix.png"
-                name="Felix"
-                phone="(212) 555-4567"
-                email="thecat@hotmail.com"
-            />
-            <Contact 
-                img="./images/pumpkin.png"
-                name="Pumpkin"
-                phone="(0800) CAT KING"
-                email="pumpkin@scrimba.com"
-            />
+        <div>
+            <Navbar />
+            <Hero />
+            <section className="cards-list">
+                {cards}
+            </section>
         </div>
     )
 }
-
-export default App
